3ed2ad1902e67786e11bb6d75a870d3b9e64295a
John Doe <johndoe@example.com>
2024-12-23 14:42:45 +0100

    Layouts, Materialize & FontAwesome

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 
-// @desc Login/landing page
+// @desc  Login/landing page
 // @route GET /
 router.get("/", (req, res) => {
-    res.send("Login");
+    res.render("login");
 })
 
-// @desc Dashboard
+// @desc  Dashboard
 // @route GET /dashboard
 router.get("/dashboard", (req, res) => {
-    res.send("Dashboard");
+    res.render("dashboard");
 })
 
 
